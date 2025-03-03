f1d8b4fc7b5888a04521271611f73ba2d8b7197f
reurnup <158298406+returnzeroup@users.noreply.github.com>
2025-03-03 09:55:44 +0300

    2

---
+++ n19/MainClass19$2.java
@@ -0,0 +1,11 @@
+package n15$n44;
+
+public class MainClass19$2 {
+	public static void main(String[] args){
+		int xs = 1;	
+		while (xs <= 512) {
+			System.out.print (xs + " ");
+			xs*=2;
+		}
+	}
+}
