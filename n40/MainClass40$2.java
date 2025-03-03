74675ff3434d7a4a554fc1cc7c6a5c5911053355
reurnup <158298406+returnzeroup@users.noreply.github.com>
2025-03-03 10:13:15 +0300

    Add files via upload

---
+++ n40/MainClass40$2.java
@@ -0,0 +1,12 @@
+package n15$n44;
+
+public class MainClass40$2 {
+	 public static void main(String[] args) {
+		 
+	        String oldString = "Java";
+	        String newString =  oldString.replace('a', 'i');
+	 
+	        System.out.println("Old string: " + oldString);
+	        System.out.println("New string: " + newString);
+	    }
+}
---
+++ n40/MainClass40$3.java
@@ -0,0 +1,12 @@
+package n15$n44;
+
+public class MainClass40$3 {
+	public static void main(String[] args) {
+		 
+        String oldString = "Java";
+        String newString =  oldString.replace("av", "oshu");
+ 
+        System.out.println("Old string: " + oldString);
+        System.out.println("New string: " + newString);
+    }
+}
